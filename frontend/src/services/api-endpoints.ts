86f655ac6d9f536a29be7c86d0fba2deafaf7899
evan <evanhugues@proton.me>
2025-08-10 20:23:52 +0200

    implement passkey authentication with registration and login functionality

---
+++ frontend/src/services/api-endpoints.ts
@@ -0,0 +1,5 @@
+// to clean for better use :)
+export const registerStart: string = '/api/passkey/registerStart';
+export const registerFinish:string = '/api/passkey/registerFinish';
+export const loginStart:string = '/api/passkey/loginStart';
+export const loginFinish:string = '/api/passkey/loginFinish';
