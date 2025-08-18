12dc9715f4df2e8d75223f31185ae66b48e19aa6
evan <evanhugues@proton.me>
2025-08-19 00:16:38 +0200

    implement login functionality with email and password authentication

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,8 +1,33 @@
-<script>
+<script lang="ts">
+    import {authClient} from "$lib/client";
+    let email = "", password = "";
+    let message = "", isError = false;
+
+
+    async function handleLogin(event: Event) {
+        event.preventDefault();
+        const { data, error } = await authClient.signIn.email({
+            email,
+            password,
+            callbackURL: "/dashboard",
+            rememberMe: false,
+        }, {
+            onSuccess: () => {
+                message = "Success";
+                isError = false;
+            },
+            onError: (ctx) => {
+                alert(ctx.error.message);
+                message = error?.message || "Login failed.";
+                isError = true;
+            },
+        });
+    }
+
 </script>
 
 <div class="bg-cover bg-center bg-no-repeat flex items-center justify-center w-full h-svh">
-	<form action="">
+	<form on:submit={handleLogin} method="POST">
 		<div class="hero bg-base-200 min-h-screen">
 			<div class="hero-content flex-col lg:flex-row-reverse">
 				<div class="text-center lg:text-left">
@@ -14,14 +39,14 @@
 				<div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
 					<div class="card-body">
 						<div class="text-center" id="message">
-							<!--p style="color: {isError ? 'red' : 'green'}">{message}</p-->
+							<p style="color: {isError ? 'red' : 'green'}">{message}</p>
 						</div>
 
 						<label class="label" for="email">Email</label>
-						<input type="email" class="input" id="email" placeholder="email" />
+						<input type="email" class="input" id="email" placeholder="email"  bind:value={email}/>
 
 						<label class="label" for="password">Password</label>
-						<input type="password" class="input" id="password" placeholder="password" />
+						<input type="password" class="input" id="password" placeholder="password" bind:value={password}/>
 
 						<button class="btn bg-gradient-to-r from-violet-300 to-blue-600 mt-4" id="loginButton"
 							>Login</button
