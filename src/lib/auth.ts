b624ef6672b27e7933cf2da9e1ea59010c437171
evan <your@email.com>
2025-08-18 18:04:22 +0200

    add schemas for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from '../index';
 
 
-
+// better Auth instance
 export const auth = betterAuth({
 	database: drizzleAdapter(db, { provider: "pg", }),
 });
